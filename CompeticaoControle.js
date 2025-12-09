342e29885a2f00c1701845a11007944eeb4d074b
HenriqueHH15 <henriquehh1511@gmail.com>
2025-12-09 10:45:11 -0300

    "Terminei" de criar o CompeticaoControle

--- CompeticaoControle.js
+++ CompeticaoControle.js
@@ -11,6 +11,7 @@ export class CompeticaoControle{
     }
 
     static removerCompeticao(nomeCompeticao){
+        this.#lstCompeticoes = localStorage.getItem("lstCompeticoes");
         let indCompeticaoParaRemover = -1;
         for(let i = 0; i < this.#lstCompeticoes.length; i++){
             if(this.#lstCompeticoes[i].nome = nomeCompeticao){
@@ -20,12 +21,15 @@ export class CompeticaoControle{
 
         if(indCompeticaoParaRemover != -1){
             this.#lstCompeticoes.splice(indCompeticaoParaRemover, 1);
+            localStorage.setItem("lstCompeticoes", JSON.stringify(this.#lstCompeticoes));
             return true;
         }
+        localStorage.setItem("lstCompeticoes", JSON.stringify(this.#lstCompeticoes));
         return false;
     }
 
     static atualizarCompeticao(nome, distancia, dataHorario, local, limiteVagas){
+        this.#lstCompeticoes = localStorage.getItem("lstCompeticoes");
         let indCompeticaoParaAtt = -1;
         for(let i = 0; i < this.#lstCompeticoes.length; i++){
             if(this.#lstCompeticoes[i].nome = nome){
@@ -35,20 +39,43 @@ export class CompeticaoControle{
 
         if(indCompeticaoParaAtt != -1){
             if(distancia != ""){
-                this.#lstCompeticoes[i].distancia = distancia;
+                this.#lstCompeticoes[indCompeticaoParaAtt].distancia = distancia;
 
             } else if(dataHorario != ""){
-                this.#lstCompeticoes[i].dataHorario = dataHorario;
+                this.#lstCompeticoes[indCompeticaoParaAtt].dataHorario = dataHorario;
 
             } else if(local != ""){
-                this.#lstCompeticoes[i].local = local;
+                this.#lstCompeticoes[indCompeticaoParaAtt].local = local;
 
             } else if(limiteVagas != 0){
-                this.#lstCompeticoes[i].limiteVagas = limiteVagas;
+                this.#lstCompeticoes[indCompeticaoParaAtt].limiteVagas = limiteVagas;
 
             }
+            localStorage.setItem("lstCompeticoes", JSON.stringify(this.#lstCompeticoes));
             return true;
         }
+        localStorage.setItem("lstCompeticoes", JSON.stringify(this.#lstCompeticoes));
+        return false;
+    }
+
+    static inscreverAtleta(nomeAtleta, classificacao, nomeCompeticao){
+        this.#lstCompeticoes = localStorage.getItem("lstCompeticoes");
+        let indCompeticao = -1;
+        for(let i = 0; i < this.#lstCompeticoes.length; i++){
+            if(this.#lstCompeticoes[i].nome = nomeCompeticao){
+                indCompeticao = i;
+            }
+        }
+
+        if(indCompeticao != -1){
+            if(this.#lstCompeticoes[indCompeticao].inscreverAtleta(nomeAtleta, classificacao)){
+                localStorage.setItem("lstCompeticoes", JSON.stringify(this.#lstCompeticoes));
+                return true;
+            }
+            localStorage.setItem("lstCompeticoes", JSON.stringify(this.#lstCompeticoes));
+            return false;
+        }
+        localStorage.setItem("lstCompeticoes", JSON.stringify(this.#lstCompeticoes));
         return false;
     }
 }
